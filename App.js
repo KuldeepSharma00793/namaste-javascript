29824f8c1aa3ec548c1ef4bb5a97cd6d9337c30e
KuldeepSharma00793 <kuldeepsharma00793@gmail.com>
2023-10-14 11:12:17 +0530

    Food ordering App

--- App.js
+++ App.js
@@ -1,38 +1,129 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Title = ()=>(
-<h1 className="head">
-    Namaste React elemement
-    </h1>
-);
+
+/*
+* Header Component
+*   -Logo item
+*   -Nav Items
+* Body Component
+*    - Search component
+*    - Restaurants Container
+*       - Restuarants Cards
+*           - img, name of res, star rating, cuisines, delivery time etc
+* Footer Component
+*   - Copyright
+*  - Links
+*   - Address 
+*   -Contact
+*/
+
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://png.pngtree.com/png-clipart/20230106/original/pngtree-simple-and-modern-food-logo-vector-design-png-image_8876455.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 
-const number = 10000;
+// styles are given as object in react
 
-// React Functional Component - A Function which is returning piece of jsx 
-const HeadingComponent = ()=>(
-    <div id="container">
-        <h2>{number}</h2>  
-        <h3>{Title()}</h3>
-        <Title />
-        <Title></Title>
-        {/* we can inject any js code inside { } and integrate it into a html like above line */}
-    
-    <h1>Namaste React Functional Component</h1>
-</div>
-);
+// const styleCard = {
+//     backgroundColor: "#f0f0f0"
+// } 
 
+
+const RestaurantCard = (props)=>{
+    const {resData} = props;
+    const {name, Cuisines, costForTwo, avgRating} = resData?.data;
+    return (
+        <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
+            <h3>{name}</h3>
+            <h4>{Cuisines.join(",")}</h4>
+            <h4>{costForTwo /10}</h4>
+            <h4>{avgRating}</h4>
+        </div>
+    );
+};
+
+const resList = [
+    {
+       data:
+       { 
+        name : "KFC",
+        id : 1,
+        Cuisines : ["Burgers", "Biryani", "American"],
+        costForTwo: 4000,
+        avgRating: "3.8"
+    }
+    },
+    {
+        data: {
+        name : "KHana Khajana",
+        id : 2,
+        Cuisines : ["North Indian", "south indian", "American"],
+        costForTwo: 5000,
+        avgRating: "4.8"
+        }
+    },
+    {
+        data : {
+        name : "KFC",
+        id : 3,
+        Cuisines : ["south", "italian", "continental"],
+        costForTwo: 6000,
+        avgRating: "4.5"
+    }},
+    {
+        data: {
+        name : "China Town",
+        id : 4,
+        Cuisines : ["Burgers", "chowmein", "dosa"],
+        costForTwo: 3000,
+        avgRating: "4.8"
+    }}
+];
+
+
+
+const Body = ()=>{
+    return (
+        <div className="body">
+            <div className="Search">Search</div>
+            <div className="res-container">
+
+               { resList.map((x)=> {
+               return <RestaurantCard key={x.data.id} resData={x} />;
+                })
+            }
+            </div>
+        </div>
+    );
+};
+
+
+const AppLayout = ()=>{
+    return (
+        <div className="app">
+            <Header />
+            <Body/>
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HeadingComponent />);
-
-
-// The above scenario composing one component into other is component composition. 
-
-
-
-
+root.render(<AppLayout />);
 
 
 
